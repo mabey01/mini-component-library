369c093c76f075b3cea1895fd493dd7915a0c48f
Maximilian Beyer <mabey01@gmail.com>
2021-11-24 10:34:55 +1000

    implement select and input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    icon: 16,
+    strokeWidth: 1,
+    borderWidth: '1px',
+    fontSize: '16px'
+  },
+  large: {
+    icon: 24,
+    strokeWidth: 2,
+    borderWidth: "2px",
+    fontSize: '18px'
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,57 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return (
+    <Container style={{'--border-width': SIZES[size].borderWidth, '--width': width + 'px', "--font-size": SIZES[size].fontSize}}>
+      <InputIcon size={SIZES[size].icon} strokeWidth={SIZES[size].strokeWidth} id={icon}/>
+      <Input type="text" placeholder={placeholder}/>
+      <VisuallyHidden>{label}</VisuallyHidden>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  border-bottom: var(--border-width) solid currentColor;
+  border-radius: 1px;
+  width: var(--width);
+  padding: 4px;
+
+  white-space: nowrap;
+
+  &:focus-within {
+    outline: 2px solid ${COLORS.primary};
+    outline-offset: 4px;
+  }
+`
+
+const InputIcon = styled(Icon)`
+  display: inline-block;
+`
+
+const Input = styled.input`
+  display: inline-block;
+  padding: 0%;
+  margin: 0 0 0 -20px;
+  width: var(--width);
+  border: none;
+  background: none;
+
+
+  color: ${COLORS.gray700};
+  font-size: var(--font-size);
+  text-indent: 25px;
+  font-weight: 700;
+  outline: none;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500}
+  }
+  
+`
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,61 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Container>
+      <SelectValue>
+        <span>{displayedValue}</span>
+        <SelectIcon size={22} strokeWidth={2} id='chevron-down'/>
+      </SelectValue>
+      <ActualSelect value={value} onChange={onChange}>
+        <label>{displayedValue}</label>
+        {children}
+      </ActualSelect>
+    </Container>
   );
 };
+
+const SelectValue = styled.div`
+  padding: 10px 12px;
+  border: none;
+  background-color: ${COLORS.transparentGray15};
+
+  display: flex;
+  align-items: center;
+`
+
+
+const SelectIcon = styled(Icon)`
+  margin-inline-start: 16px;
+`
+
+const Container = styled.div`
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+      cursor: pointer;
+  }
+
+  &:focus-within {
+    outline: 2px solid ${COLORS.primary};
+  }
+`
+
+const ActualSelect =  styled.select`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  width: 100%;
+
+  opacity: 0;
+`
 
 export default Select;
